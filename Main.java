15692e014b3986954866b3d62c87ff53277c2660
Yasmin Nunes <yasminsilvanunes663@gmail.com>
2023-04-09 19:16:38 -0300

    foi começado a etapa 2

--- Main.java
+++ Main.java
@@ -6,10 +6,12 @@ public class Main {
         int numOps = 10000;
         int numAuxNext = 0;
         LinkedListOfInteger lista = new LinkedListOfInteger();
+        ListWithCurrent lista2 = new  ListWithCurrent();
+
         Random rand = new Random();
         for (int i = 0; i < 1000; i++) {
             if (i % 1000 == 0)
-            System.out.println("Mais 1000...");
+                System.out.println("Mais 1000...");
             lista.add(rand.nextInt(1000));
         }
         System.out.println("A lista tem " + lista.size() + " elementos.");
@@ -32,7 +34,33 @@ public class Main {
 
             numAuxNext += lista.getNumAuxNext();
         }
+        System.out.println("Etapa 2");
 
-        
+        for (int i = 0; i < 1000; i++) {
+            if (i % 1000 == 0)
+                System.out.println("Mais 1000...");
+            lista2.add(rand.nextInt(1000));
+        }
+        System.out.println("A lista tem " + lista2.size() + " elementos.");
+
+        for (int i = 0; i < numOps; i++) {
+            int op = rand.nextInt(3);
+            int value = rand.nextInt(100);
+
+            switch (op) {
+                case 0:
+                    lista2.add(value, value);
+                    break;
+                case 1:
+                    lista2.get(value);
+                    break;
+                case 2:
+                    lista2.removeByIndex(value);
+                    break;
+            }
+
+            numAuxNext += lista2.getNumAuxNext();
+        }
+
     }
 }
