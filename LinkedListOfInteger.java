e3fe3494d366fbcfc54cb17061969c68fdb38465
Yasmin Nunes <yasminsilvanunes663@gmail.com>
2023-04-27 06:58:18 -0300

    ultma atu

--- LinkedListOfInteger.java
+++ LinkedListOfInteger.java
@@ -2,6 +2,10 @@
 public class LinkedListOfInteger {
     // Classe interna Node
     int NumAuxNext;
+    private Node head;
+    private Node tail;
+    private int count;
+    private Node current; 
 
     private class Node {
         public Integer element;
@@ -11,16 +15,113 @@ public class LinkedListOfInteger {
             this.element = element;
             next = null;
         }
+    }
 
-        public Node(Integer element, Node next) {
-            this.element = element;
-            this.next = next;
+    public void addcurrent(Integer element) {
+        Node n = new Node(element);
+        if (head == null) {
+            head = n;
+            current = n;
+        } else {
+            tail.next = n;
+            current = n;
+        }
+        tail = n;
+        current =n;
+        count++;
+    }
+
+    public void addcurrent(int index, Integer element) { 
+        if (index < 0 || index > size())
+            throw new IndexOutOfBoundsException(); 
+
+        Node n = new Node(element);
+        count++;
+
+        if (index == 0) { 
+            if (head == null) { 
+                tail = n;
+                current = n;
+            } else {
+                n.next = head;
+            }
+            head = n;
+            current = n;
+
+        } else if (index == count) { 
+            tail.next = n;
+            tail = n;
+            current = n;
+             } else { 
+            Node ant = head;
+            for (int i = 0; i < index - 1; i++){
+                ant = ant.next;
+                NumAuxNext++;
+            }
+            n.next = ant.next;
+            ant.next = n;
+            current = n;
+
         }
     }
 
-    private Node head;
-    private Node tail;
-    private int count;
+    public int getNumAuxNext() {
+        return NumAuxNext;
+    }
+
+    public Integer getcurrent(int index) { // O(n)
+        if ((index < 0) || (index >= count)) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == count - 1){//index igual ao tamanho -1(ou seja index vai para a tail)
+            current = tail;
+            return tail.element;}
+
+        Node aux = head;
+        int c = 0;
+        while (c < index) {
+            aux = aux.next;
+            NumAuxNext++;
+            c++;
+            current = aux;
+        }
+        return (aux.element);
+    }
+
+    public Integer removeByIndexcuent(int index) {
+        if (index < 0 || index >= size())
+            throw new IndexOutOfBoundsException(); 
+        if (index == 0) {
+            Integer elem = head.element;
+            head = head.next;
+            if (count == 1) 
+                tail = null;
+            count--; 
+            return elem;
+        }
+        Node aux = head;
+        Node ant = null;
+        for (int i = 0; i < index; i++) {
+            ant = aux;
+            current = aux;
+            aux = aux.next;
+            NumAuxNext++;
+
+        }
+        if (aux == tail) { 
+            tail = ant;
+            tail.next = null;
+        } else { 
+            ant.next = aux.next;
+            NumAuxNext++;
+            current = aux.next;
+            NumAuxNext++;
+
+        }
+        count--;
+        return aux.element;
+    }
+
 
    
     public LinkedListOfInteger() {
@@ -72,19 +173,19 @@ public class LinkedListOfInteger {
         } else if (index == count) { 
             tail.next = n;
             tail = n;
-        } else { 
+             } else { 
             Node ant = head;
-            for (int i = 0; i < index - 1; i++)
+            for (int i = 0; i < index - 1; i++){
                 ant = ant.next;
+                NumAuxNext++;
+            }
             n.next = ant.next;
             ant.next = n;
         }
     }
 
 
-    public int getNumAuxNext() {
-        return NumAuxNext;
-    }
+ 
 
     public Integer get(int index) { // O(n)
         if ((index < 0) || (index >= count)) {
@@ -135,5 +236,8 @@ public class LinkedListOfInteger {
         return aux.element;
     }
 
+    //etapa 2
+
+    
    
 }
--- Main.java
+++ Main.java
@@ -1,24 +1,44 @@
 import java.util.Random;
-
+import java.util.Scanner;
+//tem que reposizionar no meio a cada certa quantidade
+//poder posicionar o current em outras posições 
+//fazer um menu para as etapas e esse tipo de coisa
 public class Main {
     public static void main(String[] args) {
         System.out.println("Etapa 1");
         int numOps = 10000;
-        int numAuxNext = 0;
-        LinkedListOfInteger lista = new LinkedListOfInteger();
-        ListWithCurrent lista2 = new  ListWithCurrent();
-
+        int numAuxNext = 0; //quantidade de vezes que a operação aux.next é executada 
+        int opcao = 1;
         Random rand = new Random();
+
+        Scanner entrada = new Scanner(System.in);
+
+        LinkedListOfInteger lista = new LinkedListOfInteger();//lista da etapa 1
+        do{
+            System.out.println("1 - etapa 1 ");
+            System.out.println("2 - etapa 2 ");
+            System.out.println("3 - etapa 3 ");
+            System.out.println("10 - sair");
+
+
+        }while(opcao!=10);
+
+        switch(opcao){
+
+            case 1:
+            
+
         for (int i = 0; i < 1000; i++) {
-            if (i % 1000 == 0)
+            if (i % 10000 == 0)
                 System.out.println("Mais 1000...");
-            lista.add(rand.nextInt(1000));
+                lista.add(rand.nextInt(1000));
         }
         System.out.println("A lista tem " + lista.size() + " elementos.");
-
-        for (int i = 0; i < numOps; i++) {
+        System.out.print( numAuxNext = lista.getNumAuxNext());
+ 
+       for (int i = 0; i < numOps; i++) {
             int op = rand.nextInt(3);
-            int value = rand.nextInt(100);
+            int value = rand.nextInt(10000);
 
             switch (op) {
                 case 0:
@@ -30,37 +50,54 @@ public class Main {
                 case 2:
                     lista.removeByIndex(value);
                     break;
+                  
             }
-
-            numAuxNext += lista.getNumAuxNext();
+           
+            System.out.print( numAuxNext = lista.getNumAuxNext());//colocando o numeros de vezes dento de uma variavel 
         }
-        System.out.println("Etapa 2");
+            break;
+  
+            case 2:
 
-        for (int i = 0; i < 1000; i++) {
-            if (i % 1000 == 0)
-                System.out.println("Mais 1000...");
-            lista2.add(rand.nextInt(1000));
-        }
-        System.out.println("A lista tem " + lista2.size() + " elementos.");
 
-        for (int i = 0; i < numOps; i++) {
-            int op = rand.nextInt(3);
-            int value = rand.nextInt(100);
-
-            switch (op) {
-                case 0:
-                    lista2.add(value, value);
-                    break;
-                case 1:
-                    lista2.get(value);
-                    break;
-                case 2:
-                    lista2.removeByIndex(value);
-                    break;
+            for (int i = 0; i < 1000; i++) {
+                if (i % 10000 == 0)
+                    System.out.println("Mais 1000...");
+                    lista.add(rand.nextInt(1000));
             }
+            System.out.println("A lista tem " + lista.size() + " elementos.");
+            System.out.print( numAuxNext = lista.getNumAuxNext());
+     
+           for (int i = 0; i < numOps; i++) {
+                int op = rand.nextInt(3);
+                int value = rand.nextInt(10000);
+    
+                switch (op) {
+                    case 0:
+                        lista.add(value, value);
+                        break;
+                    case 1:
+                        lista.get(value);
+                        break;
+                    case 2:
+                        lista.removeByIndex(value);
+                        break;
+                      
+                }
+               
+                System.out.print( numAuxNext = lista.getNumAuxNext());//colocando o numeros de vezes dento de uma variavel 
+            }
+            break; 
 
-            numAuxNext += lista2.getNumAuxNext();
+            case 3:
+
+            break;
+
+
         }
 
-    }
-}
+
+        
+        //ETAPA 3
+        
+    }}
